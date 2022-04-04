381e7e1f2856e2547276640d961cad437d6b3e93
Zander <alexlamarche90@gmail.com>
2022-04-04 17:30:11 -0400

    finally figured out how to preload and render images. Note: next time look up where loader is referencing. I had assets folder in src/assets/ instead of in public/assets/

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,17 +1,38 @@
 import * as PIXI from 'pixi.js';
 
 export class Renderer {
-  private _application: PIXI.Application;
+  private _application: PIXI.Application; 
 
   public constructor() {
+
     this._application = new PIXI.Application({
-      backgroundColor: 0x000000,
+      backgroundColor: 0x333333,
       autoStart: true,
     });
 
     document.body.appendChild(this._application.view);
+    
   }
-  // Below shall serve as functions to render information into the application.
+
+  loadAssets(...assets:string[]):PIXI.Sprite[]{
+    const loader = PIXI.Loader.shared;
+    loader.baseUrl = "assets/";
+
+    loader.add('ball', 'assets/test.png')
+
+    loader.load((loader, resources) => {
+
+    });
+
+
+    //TODO set
+    return [];
+  }
+
+  // Exposes stage to App
+  addToStage(stageable:PIXI.DisplayObject){
+    this._application.stage.addChild(stageable);
+  }
 }
 
 export default Renderer;
